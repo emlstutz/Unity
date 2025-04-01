92f09e095f3de7b34fc1674b7ff6094ee29e03dc
Ev <e.m.l.stutz@pl.hanze.nl>
2025-04-01 20:36:18 +0200

    Updated Inventory to work with List, can now add multiple Sprites

--- Class 1C/Assets/Scripts/InventorySystem.cs
+++ Class 1C/Assets/Scripts/InventorySystem.cs
@@ -7,6 +7,8 @@ public class InventorySystem : MonoBehaviour
     public List<GameObject> inventory = new List<GameObject>();
     public Dictionary<GameObject, Sprite> visualInventory = new Dictionary<GameObject, Sprite>();
     [SerializeField] Image _img;
+    public Image _inventorySlot;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -16,6 +18,12 @@ public class InventorySystem : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        foreach (GameObject item in inventory)
+        {
+            _img.sprite = item.GetComponent<AddToInventory>()._sprite;
+        }
+        /*This solution works with adding a Sprite to a Key Value pair of Game Object and Sprite. You could expand upon this by adding more 
+         * slots or by getting the name of the saved Sprite and and the tag of the key to see which Key it is.
         if (visualInventory.Count != 0)
         {
             foreach (KeyValuePair<GameObject, Sprite> keyValue in visualInventory)
@@ -23,6 +31,7 @@ public class InventorySystem : MonoBehaviour
                 Sprite value = keyValue.Value;
                 _img.sprite = value;
             }
-        }
+
+        }*/
     }
 }
