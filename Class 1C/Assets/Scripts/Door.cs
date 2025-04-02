95428018b557e446614a5c3c1139d6d4e83900c8
emlstutz <e.m.l.stutz@pl.hanze.nl>
2025-04-02 12:24:04 +0200

    Added Door and working on logic

---
+++ Class 1C/Assets/Scripts/Door.cs
@@ -0,0 +1,35 @@
+using NUnit.Framework;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Door : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if(CompareTag("Player") == true)
+        {
+            List<GameObject> localInventory = collision.gameObject.GetComponent<InventorySystem>().inventory;
+            int inventoryLength = localInventory.Count;
+
+            for (int i = 0; i < inventoryLength; i++)
+            {
+                if (localInventory[i].tag == "KeyRed")
+                {
+                    this.gameObject.SetActive(false);    
+                }
+            }
+        }
+    }
+}
