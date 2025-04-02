ece82de379f12f9222bcf189bdc545910074bdcd
DobJalo <marugrig@gmail.com>
2025-04-02 11:29:33 +0300

    Keys can be collected

--- Class 1C/Assets/Scripts/KeyCollecting.cs
+++ Class 1C/Assets/Scripts/KeyCollecting.cs
@@ -2,15 +2,27 @@ using UnityEngine;
 
 public class KeyCollecting : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    public bool doorKey = false;
+    public GameObject textKey;
+
+
+    private void OnTriggerEnter(Collider other)
     {
-        
+        if (other.CompareTag("Key") || other.CompareTag("doorKey"))
+        {
+            if (other.CompareTag("doorKey"))
+            {
+                doorKey = true;
+            }
+
+            textKey.SetActive(true);
+            Invoke("ShowText", 3);
+            other.gameObject.SetActive(false);
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+    void ShowText()
     {
-        
+        textKey.SetActive(false);
     }
 }
