d29b1aa82ab1be9402c2ecc2a49d446589234a71
Lars <ReparableJaguarplays@gmail.com>
2025-04-03 12:26:42 +0200

    Cam Fix 01

--- Class 1C/Assets/Scripts/SecurityCamera/PatrolState.cs
+++ Class 1C/Assets/Scripts/SecurityCamera/PatrolState.cs
@@ -1,18 +1,48 @@
 using UnityEngine;
+using UnityEngine.UIElements;
 
 public class PatrolState : StateMachineBehaviour
 {
     public float cameraRotationSpeed;
-
+    public float rayMaxDirection;
+     
 
     public override void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
     {
-        base.OnStateEnter(animator, stateInfo, layerIndex);
-    }
+        //animator.GetComponent<Renderer>().material.color = Color.green;
+        Renderer[] childRenderers = animator.GetComponentsInChildren<Renderer>();
 
-    public override void OnStateUpdate(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+        foreach (Renderer renderer in childRenderers)
+        {
+            renderer.material.color = Color.green; // Change color of all child objects
+        }
+
+        //base.OnStateEnter(animator, stateInfo, layerIndex);
+    }
+   public override void OnStateUpdate(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
     {
-        base.OnStateUpdate(animator, stateInfo, layerIndex);
+
+        Debug.DrawRay(animator.transform.position, animator.transform.forward * rayMaxDirection);
+        //base.OnStateUpdate(animator, stateInfo, layerIndex);
+        animator.transform.Rotate(0, 45 * Time.deltaTime * cameraRotationSpeed, 0);
+
+        if (SeePlayer(animator))
+        {
+            Debug.Log("This works");
+            animator.SetInteger("CameraState", 1);
+        }
+
     }
 
+    private bool SeePlayer(Animator animator)
+    {
+        if (Physics.Raycast(animator.transform.position, animator.transform.forward * rayMaxDirection, out RaycastHit hitinfo))
+        {
+            if (hitinfo.transform.CompareTag("Player"))
+            {
+                return true;
+            }
+        }
+        return false;
+    }
 }
