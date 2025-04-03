4625cde45822cb00c0a9ddfc862c301aa4421a2d
Lars <ReparableJaguarplays@gmail.com>
2025-04-03 10:33:03 +0200

    Security Camera 1st Fix

---
+++ Class 1C/Assets/Scripts/SecurityCamera/PatrolState.cs
@@ -0,0 +1,18 @@
+using UnityEngine;
+
+public class PatrolState : StateMachineBehaviour
+{
+    public float cameraRotationSpeed;
+
+
+    public override void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    {
+        base.OnStateEnter(animator, stateInfo, layerIndex);
+    }
+
+    public override void OnStateUpdate(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    {
+        base.OnStateUpdate(animator, stateInfo, layerIndex);
+    }
+
+}
