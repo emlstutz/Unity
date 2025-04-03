3988096761e7498cbff8d54e1016f99980125a9b
JangoognaJ <jorannguyendenhaan@gmail.com>
2025-04-03 11:44:21 +0200

    Changes Camera

---
+++ Class 1C/Assets/PatrolState.cs
@@ -0,0 +1,30 @@
+using UnityEngine;
+
+public class PatrolState : StateMachineBehaviour
+{
+    public float rotationSpeed = 0;
+    
+
+    // OnStateEnter is called when a transition starts and the state machine starts to evaluate this state
+
+
+    public override void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    {
+        Renderer renderer = animator.gameObject.GetComponent<Renderer>();
+
+        if (renderer != null)
+        {
+            renderer.material.color = Color.green;
+        }
+
+    }
+   
+
+    // OnStateUpdate is called on each Update frame between OnStateEnter and OnStateExit callbacks
+
+    public override void OnStateUpdate(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    {
+        
+    }
+}
+    
